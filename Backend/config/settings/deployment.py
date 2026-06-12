e24437a84100881131f354cda8d7e94a72ef35f0
Elias <Elias.Benfridja06@gmail.com>
2026-06-12 12:41:28 +0100

    fixing deployment problems

--- Backend/config/settings/deployment.py
+++ Backend/config/settings/deployment.py
@@ -6,5 +6,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['resouces-search-engine-production.up.railway.app']
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        engine='django.db.backends.postgresql'
+    )
 }
