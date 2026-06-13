1f70ac73a5d62c8c58d80792775a00e1eaadde98
Elias <Elias.Benfridja06@gmail.com>
2026-06-13 19:32:19 +0100

    fix fronend deployment issues7

--- Backend/config/settings/base.py
+++ Backend/config/settings/base.py
@@ -69,7 +69,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://resouces-search-engine.vercel.app/api/resources"
+]
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
